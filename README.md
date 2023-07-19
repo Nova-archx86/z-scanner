3f685aee98ababaa882ad8153175f8132d6607df
Nova-archx86 <moorcode66@yahoo.com>
2023-07-18 21:31:01 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Disclaimer
+This software comes without any warrenty and I am not responsible
+for the way that you use this software. Scan responsibly. See LICSENCE for more info
+
+# Z-scan
+A port scanner inspired by nmap (https://nmap.org) written in zig
+
+# Installation
+
+### Building from source
+
+Requirements:
+1. zig 0.10.1
+
+Clone the repo using git and build the project using zig build
+    
+    git clone https://github.com/Nova-archx86/zscan
+    
+    cd zscan; zig build
+
---
+++ src/main.zig
@@ -0,0 +1,63 @@
+const std = @import("std");
+const net = std.net;
+const log = std.log;
+const testing = std.testing;
+
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const alloc = std.heap.page_allocator;
+const stdout = std.io.getStdOut().writer();
+
+const ScannerResult = enum {
+    OPEN,
+    CLOSED,
+    NOTSET,
+};
+
+const ScannerReport = struct {
+    host: []const u8,
+    port: u16,
+    status: ScannerResult,
+
+    fn show(self: *ScannerReport) !void {
+        const str_status = @tagName(self.status);
+        try stdout.print("Host: {s}\nPort: {any}\nStatus: {s}\n", .{ self.host, self.port, str_status });
+    }
+};
+
+fn scan(hostIp: []const u8, hostPort: u16) !ScannerReport {
+    var report = ScannerReport{ .host = hostIp, .port = hostPort, .status = ScannerResult.NOTSET };
+
+    if (net.tcpConnectToHost(alloc, hostIp, hostPort)) |res| {
+        res.close();
+        report.status = ScannerResult.OPEN;
+        return report;
+    } else |err| switch (err) {
+        error.ConnectionRefused => {
+            report.status = ScannerResult.CLOSED;
+            return report;
+        },
+
+        else => {
+            log.err("Error whlie creating tcp socket!\n", .{});
+            return err;
+        },
+    }
+}
+
+pub fn main() !void {
+    const galloc = gpa.allocator();
+    const args = try std.process.argsAlloc(galloc);
+
+    var report = try scan("192.168.0.4", 22);
+    try report.show();
+
+    std.debug.print("{s}", .{args});
+    std.process.argsFree(galloc, args);
+
+    _ = gpa.deinit();
+}
+
+test "scanner" {
+    var res = try scan("localhost", 6969);
+    try testing.expect(res.status == ScannerResult.CLOSED);
+}
