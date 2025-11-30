75c5c656c49d0a53295ed217c5683a33ac45ed1c
Nova-archx86 <moorcode66@yahoo.com>
2025-11-29 16:57:04 -0800

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Flake for z-scanner deps";
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = {self, nixpkgs}: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      buildInputs = with pkgs; [ zig_0_13 ];
+    };
+    
+  };
+}
