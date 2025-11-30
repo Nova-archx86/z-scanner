4f05e53407a3c27c5a610b5ba10a9b97c789b634
Nova-archx86 <moorcode66@yahoo.com>
2025-11-29 17:28:39 -0800

    used zon2nix to generate deps file

--- flake.nix
+++ flake.nix
@@ -2,14 +2,13 @@
   description = "Flake for z-scanner deps";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    zon2nix.url = "github:nixcommunity/zon2nix";
   };
 
   outputs = {self, nixpkgs}: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
   in {
     devShells."x86_64-linux".default = pkgs.mkShell {
-      buildInputs = with pkgs; [ zig_0_13 ];
+      buildInputs = with pkgs; [ zig_0_13 zon2nix ];
       shellHook = ''
         if [ -e $(which starship) ]; then
             eval "$(starship init bash)"
