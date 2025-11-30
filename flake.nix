5f8b151527ba587e407be1dba0dcc3c262ed7255
Nova-archx86 <moorcode66@yahoo.com>
2025-11-29 17:14:41 -0800

    added shellhook and zon2nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,20 @@
 {
   description = "Flake for z-scanner deps";
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    zon2nix.url = "github:nixcommunity/zon2nix";
+  };
 
   outputs = {self, nixpkgs}: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
   in {
     devShells."x86_64-linux".default = pkgs.mkShell {
       buildInputs = with pkgs; [ zig_0_13 ];
+      shellHook = ''
+        if [ -e $(which starship) ]; then
+            eval "$(starship init bash)"
+        fi
+      ''; 
     };
     
   };
